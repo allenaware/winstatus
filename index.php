2a8954d30006bfec30fcdc2da1bfbf98869a7bfd
allenaware <allenaware@163.com>
2016-01-30 19:54:26 +0800

    new format

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
-$date_time_array = getdate (time());
-$min = $date_time_array[ "minutes"]-3;
-if($min<0)
-{
+$date_time_array = getdate(time());
+$min = $date_time_array["minutes"] - 3;
+if ($min < 0) {
     $min = 60 + $min;
 }
 $tail = strval($min);
-if($min <10)
-{
-    $tail = '0'.$tail;
+if ($min < 10) {
+    $tail = '0' . $tail;
 
 }
-$file = file("/home/work/bidmax_monitor/data/wins_status_".$tail);
+$file = file("/home/work/bidmax_monitor/data/wins_status_" . $tail);
 $fileUnknown = file("/home/work/bidmax_monitor/data/unknown_status");
 ?>
 <title>Win Status</title>
@@ -23,104 +21,100 @@ $fileUnknown = file("/home/work/bidmax_monitor/data/unknown_status");
 <script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <head>
 
-<meta http-equiv="refresh" content="60">
+    <meta http-equiv="refresh" content="60">
 
 </head>
 
-<div class= 'well well-lg'>
- <div class="status-index">
-   <div class="row">
-        <div class="col-lg-1" >
-        </div>
-        <div class="col-lg-10">
-            <div class="panel panel-success">
-                   <div class="panel-heading">
-                          <h2 class="panel-title">Win Status</h2>
-                   </div>
-                   <div class="panel-body">
+<div class='well well-lg'>
+    <div class="status-index">
+        <div class="row">
+            <div class="col-lg-1">
+            </div>
+            <div class="col-lg-10">
+                <div class="panel panel-success">
+                    <div class="panel-heading">
+                        <h2 class="panel-title">Win Status</h2>
+                    </div>
+                    <div class="panel-body">
                         <table class="table">
-                           <thead>
-                              <tr>
-                                 <th>name</th>
-                                 <th>bids</th>
-                                 <th>events</th>
-                                 <th>wins</th>
-                                 <th>loss</th>
-                                 <th>error</th>
-                                 <th>unmatched</th>
-                              </tr>
-                           </thead>
-                           <tbody>
-<?php
-foreach($file as &$line)
-{
-    echo "<tr>";
-    $cols = explode(",",$line);
-    foreach($cols as $col)
-    {
-        echo "<td>";
-        echo $col;
-        echo "</td>";
-    }
-    echo "</tr>";
-}
+                            <thead>
+                            <tr>
+                                <th>name</th>
+                                <th>bids</th>
+                                <th>events</th>
+                                <th>wins</th>
+                                <th>loss</th>
+                                <th>error</th>
+                                <th>unmatched</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            foreach ($file as &$line) {
+                                echo "<tr>";
+                                $cols = explode(",", $line);
+                                foreach ($cols as $col) {
+                                    echo "<td>";
+                                    echo $col;
+                                    echo "</td>";
+                                }
+                                echo "</tr>";
+                            }
 
-?>
-                          </tbody>
-                     </table>
+                            ?>
+                            </tbody>
+                        </table>
 
+                    </div>
                 </div>
             </div>
-        </div>
 
-    <div class="col-lg-1">
+            <div class="col-lg-1">
 
-    </div>
-   </div>
-    <div class="row">
-        <div class="col-lg-1">
-        </div>
-        <div class="col-lg-10">
-            <div class="panel panel-info">
-                   <div class="panel-heading">
-                          <h2 class="panel-title">Unknown Status</h2>
-                   </div>
-                   <div class="panel-body">
-                        <table class="table">
-                           <thead>
-                              <tr>
-                                 <th>name</th>
-                                 <th>unknown bundle</th>
-                                 <th>unknown model</th>
-                                 <th>unknown make</th>
-                                 <th>unknown osv</th>
-                                 <th>unknown carrier</th>
-                              </tr>
-                           </thead>
-                           <tbody>
-<?php
-foreach($fileUnknown as $lineIndex=>$line)
-{
-
-    echo "<tr>";
-    $cols = explode(",",$line);
-    foreach($cols as $colIndex=>$col)
-    {
-        echo "<td>";
-        echo $col;
-        echo "</td>";
-        // echoDownloadLink($lineIndex,$colIndex);
-    }
-    echo "</tr>";
-}
-
-?>
-                          </tbody>
-                     </table>
-
-                </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-1">
+            </div>
+            <div class="col-lg-10">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h2 class="panel-title">Unknown Status</h2>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>name</th>
+                                <th>unknown bundle</th>
+                                <th>unknown model</th>
+                                <th>unknown make</th>
+                                <th>unknown osv</th>
+                                <th>unknown carrier</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            foreach ($fileUnknown as $lineIndex => $line) {
+
+                                echo "<tr>";
+                                $cols = explode(",", $line);
+                                foreach ($cols as $colIndex => $col) {
+                                    echo "<td>";
+                                    echo $col;
+                                    echo "</td>";
+                                    // echoDownloadLink($lineIndex,$colIndex);
+                                }
+                                echo "</tr>";
+                            }
+
+                            ?>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
 
         </div>
         <div class="col-lg-1">
@@ -131,28 +125,28 @@ foreach($fileUnknown as $lineIndex=>$line)
         </div>
 
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=inmobi_cat_result.csv">inmobi_cat_result.csv</a>
             </h5>
-            <h5> 
+            <h5>
         </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=inmobi_model_result.csv">inmobi_model_result.csv</a>
             </h5>
         </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=inmobi_make_result.csv">inmobi_make_result.csv</a>
             </h5>
         </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=inmobi_osv_result.csv">inmobi_osv_result.csv</a>
             </h5>
         </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=inmobi_carrier_result.csv">inmobi_carrier_result.csv</a>
             </h5>
         </div>
@@ -160,25 +154,25 @@ foreach($fileUnknown as $lineIndex=>$line)
         </div>
     </div>
     <div class="row">
-       <div class="col-lg-1">
-       </div>
+        <div class="col-lg-1">
+        </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=bes_model_result.csv">bes_model_result.csv</a>
             </h5>
         </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=bes_make_result.csv">bes_make_result.csv</a>
             </h5>
         </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=bes_osv_result.csv">bes_osv_result.csv</a>
             </h5>
         </div>
         <div class="col-lg-2">
-            <h5> 
+            <h5>
                 <a href="downloadReport.php?name=bes_carrier_result.csv">bes_carrier_result.csv</a>
             </h5>
         </div>
@@ -187,7 +181,5 @@ foreach($fileUnknown as $lineIndex=>$line)
         </div>
     </div>
 
-   </div>
-
- </div>
 </div>
+
