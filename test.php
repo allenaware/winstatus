db7bba8aa35729135e77ef50518d484f93b5bd0f
root <root@iZ11pygguouZ.(none)>
2016-09-22 13:39:35 +0800

    update

--- test.php
+++ test.php
@@ -232,31 +232,45 @@ if (file_exists('/home/work/bidmax_monitor/mon_stat.log')) {
         <div class="col-lg-10">
             <div class="panel panel-info">
                 <div class="panel-heading">
-                    <h2 class="panel-title">Log Status</h2>
+                    <h2 class="panel-title">Monitor Status 1 Hour</h2>
                 </div>
                 <div class="panel-body">
                     <table class="table">
                         <thead>
                         <tr>
-                            <th>IP</th>
-                            <th>Log Path</th>
-                            <th>Status</th>
+                            <th>Fail Rate</th>
+                            <th>Reasons</th>
+                            <th>Total Errors</th>
+                            <th>Total Pings</th>
+                            <th>Total Successes</th>
                         </tr>
                         </thead>
                         <tbody>
                         <?php
-                        foreach ($fileLogMonitor as $lineIndex => $line) {
 
                             echo "<tr>";
-                            $cols = explode("\t", $line);
-                            foreach ($cols as $colIndex => $col) {
-                                echo "<td>";
-                                echo $col;
-                                echo "</td>";
-                                // echoDownloadLink($lineIndex,$colIndex);
-                            }
+                            echo "<td>";
+                            echo $statMonitor['1h_stat']['Fail rate'];
+                            echo "</td>";
+                            echo "<td>";
+                            // echo $statMonitor['1h_stat']['Reasons'];
+                            echo "test";
+                            echo "</td>";
+                            echo "<td>";
+                            echo $statMonitor['1h_stat']['Total errors'];
+                            echo "</td>";
+
+                            echo "<td>";
+                            echo $statMonitor['1h_stat']['Total pings'];
+                            echo "</td>";
+
+                            echo "<td>";
+                            echo $statMonitor['1h_stat']['Total successes'];
+                            echo "</td>";
                             echo "</tr>";
-                        }
+
+
+
 
                         ?>
                         </tbody>
@@ -272,7 +286,62 @@ if (file_exists('/home/work/bidmax_monitor/mon_stat.log')) {
     <div class="row">
         <div class="col-lg-1">
         </div>
+        <div class="col-lg-10">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Monitor Status 24 Hour</h2>
+                </div>
+                <div class="panel-body">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Fail Rate</th>
+                            <th>Reasons</th>
+                            <th>Total Errors</th>
+                            <th>Total Pings</th>
+                            <th>Total Successes</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
 
+                            echo "<tr>";
+                            echo "<td>";
+                            echo $statMonitor['24h_stat']['Fail rate'];
+                            echo "</td>";
+                            echo "<td>";
+                            // echo $statMonitor['1h_stat']['Reasons'];
+                            echo "test";
+                            echo "</td>";
+                            echo "<td>";
+                            echo $statMonitor['24h_stat']['Total errors'];
+                            echo "</td>";
+
+                            echo "<td>";
+                            echo $statMonitor['24h_stat']['Total pings'];
+                            echo "</td>";
+
+                            echo "<td>";
+                            echo $statMonitor['24h_stat']['Total successes'];
+                            echo "</td>";
+                            echo "</tr>";
+
+
+
+
+                        ?>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-1">
+        </div>
+    </div>
+
+    <div class="row">
         <a href="./debug.php" class="btn btn-primary btn-lg" role="button">BidMax Debug</a>
         <div class="col-lg-1">
         </div>
